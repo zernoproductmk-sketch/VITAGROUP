017d635a3b20969b9f66248dd5b340e026c27579
zernoproductmk-sketch <zernoproduct.mk@gmail.com>
2026-09-23 16:19:10 +0300

    Register ERP plan API

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,7 @@ from .admin_routes import router as reference_router
 from .api import router
 from .config import settings
 from .coverse_routes import router as coverse_router
+from .erp_plan_routes import router as erp_plan_router
 from .master_data_routes import router as master_data_router
 from .yandex_disk_routes import router as yandex_disk_router
 
@@ -26,6 +27,7 @@ app.add_middleware(
 app.include_router(router)
 app.include_router(coverse_router)
 app.include_router(yandex_disk_router)
+app.include_router(erp_plan_router)
 app.include_router(master_data_router)
 app.include_router(reference_router)
 
